43e9df62e8c8013631c30ef18f3a91275dced4d5
EdgarTip <edgartip2003@gmail.com>
2021-10-06 22:11:24 +0100

    Commited

--- Código/teste.go
+++ Código/teste.go
@@ -7,6 +7,6 @@ return n * factorial (n -1);
 };
 func main () {
 var argument int ;
-argument , _ = strconv.Atoi ( os.Args [1]);
-fmt.Println ( factorial ( argument ));
+argument , _ = strconv . Atoi ( os . Args [1]);
+fmt . Println ( factorial ( argument ));
 };
