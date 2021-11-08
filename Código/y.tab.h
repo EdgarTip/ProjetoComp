5be33990a814f3e5d2634b686aa07ccb1563deb6
EdgarTip <edgartip2003@gmail.com>
2021-11-08 23:42:26 +0000

    Returns implemented

--- Código/y.tab.h
+++ Código/y.tab.h
@@ -139,7 +139,17 @@ extern int yydebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef int YYSTYPE;
+union YYSTYPE
+{
+#line 20 "gocompiler.y"
+
+int value;
+char* id;
+
+#line 150 "y.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
 #endif
